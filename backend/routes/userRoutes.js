721feb0cf976da08c2606a617e0f5f631a651a0a
Marcin Maciąg <01159356@pw.edu.pl>
2023-01-10 13:03:37 +0100

    small fixes, data changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -117,6 +117,72 @@ const offerts = [
   ),
 ];
 
+const offerts2 = [
+  createData(
+    "Car",
+    "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200",
+    20,
+    30,
+    40
+  ),
+  createData(
+    "Black car",
+    "https://imageio.forbes.com/specials-images/imageserve/5f962d31e7b04bbfd2f68758/Bugatti-Chiron-Super-Sport-300--Driving/960x0.jpg?height=473&width=711&fit=bounds",
+    5,
+    56,
+    43
+  ),
+  createData(
+    "Audi",
+    "https://www.topgear.com/sites/default/files/2022/07/5_1.jpg",
+    10,
+    30,
+    20
+  ),
+  createData(
+    "Ferrari",
+    "https://cdn.carsandcoffee.com.sg/web/bo/home-feature/jrkjodVGppgzo5s5EGBG/all-ferrari-sf90-cars-and-coffee-singapore_1440x1440.jpeg?alt=media&token=5b4e1232-9bbb-4f7c-9156-e1decc58e79a",
+    55,
+    32,
+    32
+  ),
+  createData(
+    "Audi Modern",
+    "https://i.ytimg.com/vi/dip_8dmrcaU/maxresdefault.jpg",
+    44,
+    23,
+    432
+  ),
+  createData(
+    "Bugatti",
+    "https://www.bugatti.com/fileadmin/_processed_/e/e/csm_og-image_b5397aa5e3.jpg",
+    10,
+    23,
+    99
+  ),
+  createData(
+    "Audi Yello 2",
+    "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Mercedes-Benz-A-class-Hatchback-210920221745.jpg&w=350&h=251&q=90&c=1",
+    42,
+    434,
+    432
+  ),
+  createData(
+    "Book",
+    "https://d2j6dbq0eux0bg.cloudfront.net/images/24595415/1297476103.jpg",
+    32,
+    434,
+    432
+  ),
+  createData(
+    "Smartfon",
+    "https://image.ceneostatic.pl/data/products/105809880/i-redmi-note-10-pro-6-128gb-szary.jpg",
+    10,
+    434,
+    432
+  ),
+];
+
 userRouter.post("/get_data", auth, (req, res) => {
   if (req.user.id === "marcin") {
     const data = {
@@ -317,7 +383,181 @@ userRouter.post("/get_data", auth, (req, res) => {
           { score: 1, name: "Franek", comment: "Very slow" },
           { score: 2, name: "Szymon", comment: "Never Again" },
         ],
+        offerts: offerts2,
+        orders: [
+          {
+            date: "2023-1-4 01:33",
+            "Sold items": 10,
+            Cycle: 10,
+            status: "Not paid",
+          },
+          {
+            date: "2023-2-3 01:33",
+            "Sold items": 20,
+            Cycle: 12,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-12-12 01:33",
+            "Sold items": 20,
+            Cycle: 12,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-3-11 01:33",
+            "Sold items": 40,
+            Cycle: 13,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 01:33",
+            "Sold items": 80,
+            Cycle: 12,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-8-10 01:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not paid",
+          },
+          {
+            date: "2023-10-13 01:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-11-5 01:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-14 07:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-5-18 06:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not paid",
+          },
+          {
+            date: "2023-6-15 04:33",
+            "Sold items": 42,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 04:33",
+            "Sold items": 22,
+            Cycle: 7,
+            status: "Returned",
+          },
+          {
+            date: "2023-1-9 01:33",
+            "Sold items": 42,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 04:33",
+            "Sold items": 42,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-10 13:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 14:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Returned",
+          },
+          {
+            date: "2023-1-13 12:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 03:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-10 18:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Returned",
+          },
+          {
+            date: "2023-1-10 21:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Returned",
+          },
+        ],
+      },
+    };
+
+    return res.send(data);
+  }
+
+  if (req.user.id === "john") {
+    const data = {
+      Allegro: {
+        aspects: {
+          Price: 9,
+          Speed: 10,
+          Kidness: 10,
+          "Service Quality": 10,
+          Communication: 9,
+          Proffesional: 10,
+        },
+        advices: [
+          "You are getting more and more negative opinions. Watch out!",
+          "Product Table is getting more and more popular",
+        ],
         offerts,
+        reviews: [
+          { score: 3, name: "Marcin Snow", comment: "Very slow" },
+          { score: 5, name: "Maciej", comment: "Awensome" },
+          { score: 2, name: "Marcin", comment: "Horrible" },
+          { score: 4, name: "Harry Potter", comment: "Very slow" },
+          {
+            score: 5,
+            name: "Piotr Nowak",
+            comment:
+              "Fast, professional and elegant. I am very happy and satisdied",
+          },
+          {
+            score: 3,
+            name: "Beric Dondarrion",
+            comment: "Taling to fast and cant understand what he was sayin'",
+          },
+          {
+            score: 1,
+            name: "Logen Ninefingers",
+            comment: "I am very disappointed.😣",
+          },
+          {
+            score: 2,
+            name: "Celebrimbor",
+            comment: "Wort salesman ever. Very not recommended.",
+          },
+          { score: 2, name: "Tattersail", comment: "He sold me broken thing." },
+          { score: 1, name: "Granny Weatherwax", comment: "Very slow" },
+          { score: 2, name: "Szymon", comment: "Never Again" },
+        ],
         orders: [
           {
             date: "2023-1-4 01:33",
@@ -329,49 +569,49 @@ userRouter.post("/get_data", auth, (req, res) => {
             date: "2023-2-5 01:33",
             "Sold items": 20,
             Cycle: 12,
-            status: "Not delivered",
+            status: "Not paid",
           },
           {
             date: "2023-12-13 01:33",
             "Sold items": 20,
             Cycle: 12,
-            status: "Not delivered",
+            status: "Not paid",
           },
           {
             date: "2023-3-13 01:33",
             "Sold items": 40,
             Cycle: 13,
-            status: "Not delivered",
+            status: "Not paid",
           },
           {
             date: "2023-1-13 01:33",
             "Sold items": 80,
             Cycle: 12,
-            status: "Not delivered",
+            status: "Returned",
           },
           {
             date: "2023-8-6 01:33",
             "Sold items": 100,
             Cycle: 11,
-            status: "Not paid",
+            status: "Returned",
           },
           {
             date: "2023-10-4 01:33",
             "Sold items": 100,
             Cycle: 11,
-            status: "Not delivered",
+            status: "Not paid",
           },
           {
             date: "2023-11-3 01:33",
             "Sold items": 100,
             Cycle: 11,
-            status: "Not delivered",
+            status: "Returned",
           },
           {
             date: "2023-1-2 01:33",
             "Sold items": 100,
             Cycle: 11,
-            status: "Not delivered",
+            status: "Not paid",
           },
           {
             date: "2023-5-13 01:33",
@@ -383,7 +623,7 @@ userRouter.post("/get_data", auth, (req, res) => {
             date: "2023-6-18 01:33",
             "Sold items": 42,
             Cycle: 11,
-            status: "Not delivered",
+            status: "Not paid",
           },
           {
             date: "2023-1-5 01:33",
@@ -395,7 +635,7 @@ userRouter.post("/get_data", auth, (req, res) => {
             date: "2023-1-3 01:33",
             "Sold items": 42,
             Cycle: 2,
-            status: "Not delivered",
+            status: "Returned",
           },
           {
             date: "2023-1-8 01:33",
@@ -407,13 +647,13 @@ userRouter.post("/get_data", auth, (req, res) => {
             date: "2023-1-2 01:33",
             "Sold items": 100,
             Cycle: 2,
-            status: "Not delivered",
+            status: "Returned",
           },
           {
             date: "2023-1-2 01:33",
             "Sold items": 100,
             Cycle: 2,
-            status: "Returned",
+            status: "Not delivered",
           },
           {
             date: "2023-1-6 02:33",
@@ -425,16 +665,174 @@ userRouter.post("/get_data", auth, (req, res) => {
             date: "2023-1-6 03:33",
             "Sold items": 100,
             Cycle: 2,
-            status: "Not delivered",
+            status: "Returned",
           },
           {
             date: "2023-1-6 04:33",
             "Sold items": 100,
             Cycle: 2,
-            status: "Returned",
+            status: "Not delivered",
           },
           {
             date: "2023-1-6 21:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+        ],
+      },
+      Amazon: {
+        aspects: {
+          Price: 1,
+          Speed: 1,
+          Kidness: 1,
+          "Service Quality": 2,
+          Communication: 3,
+          Proffesional: 1,
+        },
+        advices: [
+          "You are getting more and more negative opinions. Watch out!",
+          "Product Table is getting more and more popular",
+        ],
+        reviews: [
+          { score: 5, name: "Tomasz", comment: "Great" },
+          { score: 3, name: "Maciej", comment: "What a nightmare" },
+          { score: 5, name: "Zuzanna", comment: "Love the service" },
+          { score: 1, name: "Harry Potter", comment: "Very slow" },
+          {
+            score: 1,
+            name: "Winon",
+            comment: "Super wrong offerts, a lot of mistakes",
+          },
+          { score: 5, name: "Random Guy", comment: "Fantastic" },
+          {
+            score: 5,
+            name: "Mariusz",
+            comment: "Amazing service, best salseman",
+          },
+          { score: 5, name: "Marcin Snow", comment: "Super great" },
+          { score: 5, name: "Piotr Nowak", comment: "Best man" },
+          { score: 5, name: "Franek", comment: "My must pick" },
+          { score: 2, name: "Szymon", comment: "Never Again" },
+        ],
+        offerts,
+        orders: [
+          {
+            date: "2023-1-4 01:33",
+            "Sold items": 10,
+            Cycle: 10,
+            status: "Not paid",
+          },
+          {
+            date: "2023-2-3 01:33",
+            "Sold items": 20,
+            Cycle: 12,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-12-12 01:33",
+            "Sold items": 20,
+            Cycle: 12,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-3-11 01:33",
+            "Sold items": 40,
+            Cycle: 13,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 01:33",
+            "Sold items": 80,
+            Cycle: 12,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-8-10 01:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not paid",
+          },
+          {
+            date: "2023-10-13 01:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-11-5 01:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-14 07:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-5-18 06:33",
+            "Sold items": 100,
+            Cycle: 11,
+            status: "Not paid",
+          },
+          {
+            date: "2023-6-15 04:33",
+            "Sold items": 42,
+            Cycle: 11,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 04:33",
+            "Sold items": 22,
+            Cycle: 7,
+            status: "Returned",
+          },
+          {
+            date: "2023-1-9 01:33",
+            "Sold items": 42,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 04:33",
+            "Sold items": 42,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-10 13:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 14:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Returned",
+          },
+          {
+            date: "2023-1-13 12:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-11 03:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Not delivered",
+          },
+          {
+            date: "2023-1-10 18:33",
+            "Sold items": 100,
+            Cycle: 2,
+            status: "Returned",
+          },
+          {
+            date: "2023-1-10 21:33",
             "Sold items": 100,
             Cycle: 2,
             status: "Returned",
--- frontend/src/components/atomic/Advice.jsx
+++ frontend/src/components/atomic/Advice.jsx
@@ -2,7 +2,7 @@ import ideaFot from "../../assets/idea.png";
 
 const Advice = ({ advice }) => {
   return (
-    <section className="flex flex-row items-center justify-center gap-[1rem] rounded-3xl bg-[#023E8A] xl:w-[50%] py-[1.5rem] px-[1rem] sm:w-[90%]">
+    <section className="flex flex-row  items-center justify-center gap-[1rem] rounded-3xl bg-[#023E8A] xl:w-[50%] py-[1.5rem] px-[1rem] sm:w-[90%]">
       <img alt="fot" src={ideaFot} />
       <h2 className="text-[white] font-bold">{advice}</h2>
     </section>
